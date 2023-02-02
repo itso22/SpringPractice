f2f5d6327df5757296e0a7b77e4ff4b7a709c00b
soyeon <itso22@naver.com>
2023-02-02 16:30:07 +0900

    feat: 파일업로드

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -20,9 +21,9 @@ public class BoardController {
     }
 
     @PostMapping("/board/writePro")
-    public String boardWritePro(Board board, Model model){
+    public String boardWritePro(Board board, Model model , MultipartFile file) throws Exception{
 
-        boardService.write(board);
+        boardService.write(board,file);
 
         model.addAttribute("message", "글 작성이 완료되었습니다");
         model.addAttribute("searchUrl", "/board/list");
@@ -60,13 +61,13 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
-        boardService.write(boardTemp);
+        boardService.write(boardTemp, file);
         return "redirect:/board/list";
     }
 
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -4,8 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -14,8 +17,20 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     //글작성
-    public void write(Board board){
+    public void write(Board board , MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "/src/main/webapp/";
+
+        UUID uuid = UUID.randomUUID();
+        String fileName = uuid + "_" + file.getOriginalFilename();
+        File saveFile = new File(projectPath, fileName);
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/webapp/" + fileName);
+        
         boardRepository.save(board);
+
     }
 
     //게시글 list 처리
