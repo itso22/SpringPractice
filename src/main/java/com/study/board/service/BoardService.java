abecfa86de2319b64d8fcddecbbfdfb7e0a401d5
soyeon <itso22@naver.com>
2023-02-02 16:46:03 +0900

    feat: 페이징처리

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,14 +30,14 @@ public class BoardService {
 
         board.setFilename(fileName);
         board.setFilepath("/webapp/" + fileName);
-        
+
         boardRepository.save(board);
 
     }
 
     //게시글 list 처리
-    public List<Board> boardList(){
-        return boardRepository.findAll();
+    public Page<Board> boardList(Pageable pageable){
+        return boardRepository.findAll(pageable);
     }
 
     //특정 게시글 불러오기
